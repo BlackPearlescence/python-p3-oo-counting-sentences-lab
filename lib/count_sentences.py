297d6613dd659309f8ede4d68f6e0e176a946bbf
Sani Rogers <sanirogers138@gmail.com>
2023-02-21 15:30:46 -0500

    do count sentences without issues

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -2,45 +2,39 @@
 import re
 
 class MyString:
+  __value = ""
   def __init__(self,value=""):
-    if(type(value) == str):
+      self.__value = value
+
+  def set_value(self,value):
+    if(type(value) is not str):
       print("The value must be a string.")
     else:
-      self.value = value
+      self.__value = value
 
-  # def set_value(self,value):
-  #   if(type(value) is not str):
-  #     print("The value must be a string.")
-  #     self._value = "The value must be a string."
-
-
-  #   else:
-  #     self.value = value
-  # def get_value(self):
-  #   return self.value
+  def get_value(self):
+    return self.__value
 
   def is_sentence(self):
-    if(self.value[-1] == "."):
+    if(self.__value[-1] == "."):
       return True
     else:
       return False
   def is_question(self):
-    if(self.value[-1] == "?"):
+    if(self.__value[-1] == "?"):
       return True
     else:
       return False
   def is_exclamation(self):
-    if(self.value[-1] == "!"):
+    if(self.__value[-1] == "!"):
       return True
     else:
       return False
   def count_sentences(self):
-    sentences = re.split('[.!?]',self.value)
+    sentences = re.split('[.!?]',self.__value)
     for i in range(sentences.count("")):
       sentences.remove("")
     print(sentences)
     return len(sentences)
+  value = property(get_value,set_value,)
 
-
-string = MyString(3000)
-string.value = 3000
