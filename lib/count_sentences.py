ae02463c5b4d24130dccfda141cdc5b31c0271e8
Sani Rogers <sanirogers138@gmail.com>
2023-02-21 14:52:51 -0500

    do but test is messed up

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,46 @@
 #!/usr/bin/env python3
+import re
 
 class MyString:
-  pass
+  def __init__(self,value=""):
+    if(type(value) == str):
+      print("The value must be a string.")
+    else:
+      self.value = value
+
+  # def set_value(self,value):
+  #   if(type(value) is not str):
+  #     print("The value must be a string.")
+  #     self._value = "The value must be a string."
+
+
+  #   else:
+  #     self.value = value
+  # def get_value(self):
+  #   return self.value
+
+  def is_sentence(self):
+    if(self.value[-1] == "."):
+      return True
+    else:
+      return False
+  def is_question(self):
+    if(self.value[-1] == "?"):
+      return True
+    else:
+      return False
+  def is_exclamation(self):
+    if(self.value[-1] == "!"):
+      return True
+    else:
+      return False
+  def count_sentences(self):
+    sentences = re.split('[.!?]',self.value)
+    for i in range(sentences.count("")):
+      sentences.remove("")
+    print(sentences)
+    return len(sentences)
+
+
+string = MyString(3000)
+string.value = 3000
